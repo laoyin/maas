02cb039bad4e84b9d89a2397d1e3f6125c34fee4
yinxingpan <yinxingpan@tmlsystem.com>
2018-04-15 17:32:17 +0800

    fix bug

--- src/maasserver/middleware.py
+++ src/maasserver/middleware.py
@@ -61,6 +61,7 @@ RETRY_AFTER_SERVICE_UNAVAILABLE = 10
 SIMPLESTREAMS_URL_PREFIX = '/images-stream/'
 
 PUBLIC_URL_PREFIXES = [
+    reverse('webserver'),
     # Login page: must be visible to anonymous users.
     reverse('login'),
     # Authentication: must be visible to anonymous users.
