8ec039c6f71cbe7b1e87ad6a93d80d53e0187283
yinxingpan <yinxingpan@tmlsystem.com>
2018-04-16 10:27:51 +0800

    fix bug

--- src/maasserver/djangosettings/monkey.py
+++ src/maasserver/djangosettings/monkey.py
@@ -21,3 +21,24 @@ def patch_get_script_prefix():
     except ImportError:
         from django.core import urlresolvers
         urlresolvers._prefixes = type('', (), {})()
+
+    try:
+        fix_django_big_auto_field()
+    except Exception as e:
+        print(str(e))
+
+# fix django version problem
+# author:yxp
+
+def fix_django_big_auto_field():
+    import django.db.models
+    # from django.db import connection
+    from django.utils.translation import ugettext_lazy as _
+    class BigAutoField(django.db.models.AutoField):
+        description = _("Big (8 byte) integer")
+        def get_internal_type(self):
+            return "BigAutoField"
+        def rel_db_type(self, connection):
+            return django.db.models.BigIntegerField().db_type(connection=connection)
+
+    django.db.models.BigAutoField = BigAutoField
